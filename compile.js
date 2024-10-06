2b3cf2b6d6fb3ed8cea113d2e9d23e47bc9cc8d8
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-10-06 10:08:54 -0400

    feat: add compile

--- compile.js
+++ compile.js
@@ -1,1 +1,8 @@
-// compile code will go here
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const inboxPah = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPah, "utf8");
+
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
