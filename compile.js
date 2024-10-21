1ee6d217cc75de8dc1b9ac1fd6c0326c94efb7a4
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-10-20 21:20:27 -0400

    chore: update to solc 0.8.19

--- compile.js
+++ compile.js
@@ -2,7 +2,25 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const inboxPah = path.resolve(__dirname, "contracts", "Inbox.sol");
-const source = fs.readFileSync(inboxPah, "utf8");
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
 
-module.exports = solc.compile(source, 1).contracts[":Inbox"];
+const input = {
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+  "Inbox.sol"
+].Inbox;
--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const { Web3 } = require("web3");
-const { interface, bytecode } = require("./compile");
+const { abi, evm } = require("./compile");
 
 const provider = new HDWalletProvider(
   "journey wheel demise taste agent shoulder tuna car soon face melt barrel",
@@ -14,9 +14,9 @@ const web3 = new Web3(provider);
 
   console.log("Attempting to deploy from account", accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
+  const result = await new web3.eth.Contract(abi)
     .deploy({
-      data: bytecode,
+      data: evm.bytecode.object,
       arguments: ["Hi there!"],
     })
     .send({ gas: "1000000", from: accounts[0] });
